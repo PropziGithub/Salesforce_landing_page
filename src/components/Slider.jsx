3de9a5cd1d3c6e28049bd91f057de656549043a4
Raphael Limbikani Mbewe <rmbewe68@gmail.com>
2022-08-22 16:54:45 -0700

    chore: add responsive slides

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -18,6 +18,32 @@ const IconSlider = () => {
         speed: 1000,
         slidesToShow: 3,
         slidesToScroll: 2,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                initialSlide: 3
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+              }
+            }
+          ]
     };
 
     const company_logo_name = [
